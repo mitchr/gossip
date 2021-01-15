2565c3828fcfeeb5be92101cab2f29f606de7842
mitchr <m.riley@temple.edu>
2021-01-15 15:37:44 -0500

    defer after error checking

--- server/server_test.go
+++ server/server_test.go
@@ -6,11 +6,20 @@ import (
 
 func TestNewServer(t *testing.T) {
 	s, err := New(":6667")
-	defer s.Listener.Close()
-
 	if err != nil {
 		t.Error(err)
 	}
+	defer s.Listener.Close()
+}
+
+func TestStartServer(t *testing.T) {
+	s, err := New(":6667")
+	if err != nil {
+		t.Error(err)
+	}
+	defer s.Listener.Close()
+
+	go s.Start()
 }
 
 func TestStartServer(t *testing.T) {
