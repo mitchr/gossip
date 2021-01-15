74fc0cb82e169c51b0711248b3c64b0ec869d42b
mitchr <m.riley@temple.edu>
2021-01-14 20:38:13 -0500

    Clean up main

--- main.go
+++ main.go
@@ -1,108 +1,24 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
+	"flag"
 	"log"
-	"net"
 
-	"github.com/mitchr/gossip/client"
+	"github.com/mitchr/gossip/server"
 )
 
+// default port of 8080
+var port *string = flag.String("port", ":8080", "sets server port")
+
 func main() {
-	l, err := net.Listen("tcp", ":6667")
+	flag.Parse()
+
+	s, err := server.New(*port)
+	defer s.Close()
+
 	if err != nil {
-		fmt.Println("could not create server on :6667")
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
-	defer l.Close()
-
-	clients := &client.List{}
-	for {
-		// wait for a connection to the server
-		// (block until one is received)
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("accepted connection")
-
-		go func(c net.Conn) {
-			// create entry for user
-			u := client.New(c.RemoteAddr(), c)
-			clients.Add(u)
-
-			// go func(c *client.Client) {
-			// 	for {
-			// 		select {
-			// 		case <-c.PING(l.Addr()):
-			// 			fmt.Println("received PONG!")
-			// 		case <-time.After(5 * time.Second):
-			// 			fmt.Println("client missed PONG")
-			// 		}
-			// 	}
-			// }(u)
-
-			// fmt.Println(c.LocalAddr().Network(), c.LocalAddr().String())
-			// fmt.Println(c.RemoteAddr().Network(), c.RemoteAddr().String())
-
-			reader := bufio.NewReader(c)
-			for {
-				// read until we encounter a newline
-				// really we should have \r\n, but we allow the parser to check that \r exists
-				msgBuf, err := reader.ReadBytes('\n')
-
-				if err != nil {
-					if err == io.EOF {
-						// client has closed connection, so we need to remove them from the user list
-						u.Close()
-						clients.Remove(u)
-						return
-					} else if operr, ok := err.(*net.OpError); ok {
-						// there was some kind of network error
-						u.Close()
-						clients.Remove(u)
-						fmt.Println(operr)
-						return
-
-					} else {
-						// not sure what happened!
-						u.Close()
-						clients.Remove(u)
-						fmt.Println(err)
-						return
-					}
-				}
-				// write to server
-				// should probably also store in a log of some kind
-				// fmt.Println(string(msgBuf))
-				// write to client
-				// conn.Write(msgBuf)
-
-				err = Parse(msgBuf, u)
-				if err != nil {
-					if err == io.EOF {
-						u.Close()
-						clients.Remove(u)
-						return
-					} else {
-						u.Write([]byte(fmt.Sprintln(err)))
-					}
-				}
-
-				// err = u.Ping(l.Addr())
-				// if err != nil {
-				// 	fmt.Println(err)
-				// }
-
-				// write message to all clients who are connected
-				// for i := 0; i < clients.Len(); i++ {
-				// 	clients.Get(i).Write(msgBuf)
-				// }
-			}
-		}(conn)
-	}
+	s.Start()
 }
