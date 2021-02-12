ad4baac04b4a40f5de0d68366eb3b2137260141c
mitchr <m.riley@temple.edu>
2021-02-12 14:00:42 -0500

    Channel.Equals can accept string
    Every channel should have a unique identifier, so we can also match on
    that.

--- channel/channel.go
+++ channel/channel.go
@@ -29,12 +29,18 @@ func New(name string, t ChanType) *Channel {
 	return &Channel{Name: name, ChanType: t}
 }
 
+// Equals accepts two types of arguments: a Channel struct, or a string.
+// If given a channel struct, it will compare against the name and type,
+// whereas if given a string of the form "#chan" or "&chan" it will
+// deduce the correct chan type and name
 func (c Channel) Equals(i interface{}) bool {
 	switch v := i.(type) {
 	case Channel:
 		return c.Name == v.Name && c.ChanType == v.ChanType
+	case string:
+		return c.Name == v[1:] && c.ChanType == ChanType(v[0])
 	default:
-		return c.Name == v
+		return false
 	}
 }
 
