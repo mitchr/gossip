60d9e3ccd742e78f10eca8eee1f484cbc2f12aa4
mitchr <m.riley@temple.edu>
2021-02-13 11:36:51 -0500

    Remove unneccessary comments

--- server/server.go
+++ server/server.go
@@ -58,22 +58,6 @@ func (s *Server) handleClient(c *client.Client) {
 	// create entry for user
 	s.Clients.Add(c)
 
-	// when a client is added, the registrationg process must be attempted
-
-	// go func(c *client.Client) {
-	// 	for {
-	// 		select {
-	// 		case <-c.PING(l.Addr()):
-	// 			fmt.Println("received PONG!")
-	// 		case <-time.After(5 * time.Second):
-	// 			fmt.Println("client missed PONG")
-	// 		}
-	// 	}
-	// }(u)
-
-	// fmt.Println(c.LocalAddr().Network(), c.LocalAddr().String())
-	// fmt.Println(c.RemoteAddr().Network(), c.RemoteAddr().String())
-
 	reader := bufio.NewReader(c)
 	for {
 		// read until we encounter a newline
@@ -96,11 +80,6 @@ func (s *Server) handleClient(c *client.Client) {
 			s.Clients.Remove(c)
 			return
 		}
-		// write to server
-		// should probably also store in a log of some kind
-		// fmt.Println(string(msgBuf))
-		// wris.te to client
-		// conn.Write(msgBuf)
 
 		msg := parse(lex(msgBuf))
 		if msg == nil {
@@ -108,26 +87,5 @@ func (s *Server) handleClient(c *client.Client) {
 		} else {
 			s.executeMessage(msg, c)
 		}
-
-		// err = s.Parse(msgBuf, c)
-		// if err != nil {
-		// 	if err == io.EOF {
-		// 		c.Close()
-		// 		s.Clients.Remove(c)
-		// 		return
-		// 	} else {
-		// 		c.Write(err)
-		// 	}
-		// }
-
-		// err = u.Ping(l.Addr())
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-
-		// write message to all clients who are connected
-		// for i := 0; i < clients.Len(); i++ {
-		// 	clients.Get(i).Write(msgBuf)
-		// }
 	}
 }
