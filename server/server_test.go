be3c5791c833c3d4f679821ff75f106ba024341c
mitchr <m.riley@temple.edu>
2021-02-14 20:06:22 -0500

    Refactor test of client registration
    We don't actually have to check the state of the server after every
    action, we can just send all the requests we need from the client and
    then check the server's state at the end to make sure everything is
    correct. This also removes the need for the weird channel/goroutine
    polling system (which was racy).

--- server/server_test.go
+++ server/server_test.go
@@ -16,7 +16,6 @@ func TestServer(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer s.Close()
-
 	go s.Serve()
 
 	conn, err := net.Dial("tcp", ":6667")
@@ -25,93 +24,49 @@ func TestServer(t *testing.T) {
 	}
 	defer conn.Close()
 
-	t.Run("AcceptClient", func(t *testing.T) {
-		// start new goroutine that continually checks the client field for any changes in length
-		ch := make(chan bool)
-		go func() {
-			for {
-				if len(s.Clients) != 0 {
-					ch <- true
-					return
-				}
-			}
-		}()
-
-		// if client was not accepted, fail all subtests
-		if !waitForChange(ch) {
-			t.Fatal("No client connection made")
-		}
-
-		// grab client from server
-		c := s.Clients.Get(0).(*client.Client)
-
-		t.Run("NICK", func(t *testing.T) {
-			n, err := conn.Write([]byte("NICK alice\r\n"))
-			if n <= 0 || err != nil {
+	t.Run("RegisterClient", func(t *testing.T) {
+		_, err := conn.Write([]byte("NICK alice\r\n"))
+		if err != nil {
 				t.Error(err)
 			}
 
-			ch := make(chan bool)
-			go func() {
-				for {
-					if c.Nick != "" {
-						ch <- true
-					}
-				}
-			}()
-
-			if !waitForChange(ch) {
-				t.Error("Nick not registered")
-			} else if c.Nick != "alice" {
-				t.Errorf("Nick registered incorrectly. Got %s\n", c.Nick)
-			}
-		})
-
-		t.Run("USER", func(t *testing.T) {
-			n, err := conn.Write([]byte("USER alice 0 * :Alice Smith\r\n"))
-			if n <= 0 || err != nil {
+		_, err = conn.Write([]byte("USER alice 0 * :Alice Smith\r\n"))
+		if err != nil {
 				t.Error(err)
-			}
-
-			ch := make(chan bool)
-			go func() {
-				for {
-					if c.User != "" {
-						ch <- true
-					}
-				}
-			}()
-
-			if !waitForChange(ch) {
-				t.Error("User not registered")
-			} else if c.User != "alice" || c.Realname != "Alice Smith" {
-				t.Errorf("User registered incorrectly. Got %s\n", c.User)
-			}
-		})
-
-		t.Run("RegisterClient", func(t *testing.T) {
-			if !c.Registered {
-				t.Fatal("Client not registered")
 			}
 
 			r := bufio.NewReader(conn)
-			t.Run("RSP_WELCOME", func(t *testing.T) {
 				welcome, err := r.ReadBytes('\n')
 				if err != nil {
 					t.Error(err)
 				}
 				fmt.Println(string(welcome))
-			})
 
-			t.Run("RSP_YOURHOST", func(t *testing.T) {
 				host, err := r.ReadBytes('\n')
 				if err != nil {
 					t.Error(err)
 				}
 				fmt.Println(string(host))
-			})
+
+		// check to see if server is in correct state
+		c := s.Clients.Get(0).(*client.Client)
+
+		if c == nil {
+			t.Fatal("No client connection made")
+		}
+		if c.Nick != "alice" {
+			t.Errorf("Nick registered incorrectly. Got %s\n", c.Nick)
+		}
+		if c.User != "alice" {
+			t.Errorf("User registered incorrectly. Got %s\n", c.User)
+		}
+		if c.Realname != "Alice Smith" {
+			t.Errorf("Real name registered incorrectly. Got %s\n", c.Realname)
+		}
+		if !c.Registered {
+			t.Error("Client not registered")
+		}
 		})
-	})
 }
 
 func Test100Clients(t *testing.T) {
