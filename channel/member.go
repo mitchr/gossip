596c70e83ca8b49fb79322480a47d2fa6a46608f
mitchr <m.riley@temple.edu>
2021-03-16 21:24:43 -0400

    Rename Member.prefix to Mode

--- channel/member.go
+++ channel/member.go
@@ -11,7 +11,7 @@ import (
 // Clients, have the capability to be given a mode/prefix.
 type Member struct {
 	*client.Client
-	prefixes string
+	Mode string
 }
 
 func NewMember(c *client.Client, p string) *Member {
@@ -21,9 +21,9 @@ func NewMember(c *client.Client, p string) *Member {
 func (m *Member) ApplyMode(mode mode.Mode) bool {
 	if r, ok := memberLetter[mode.ModeChar]; ok {
 		if mode.Add {
-			m.prefixes += string(r)
+			m.Mode += string(r)
 		} else {
-			m.prefixes = strings.Replace(m.prefixes, string(r), "", -1)
+			m.Mode = strings.Replace(m.Mode, string(r), "", -1)
 		}
 	} else {
 		return false
