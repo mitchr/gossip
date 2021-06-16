34fd8a78267cdc209974ad1b993b29af4722cf45
mitchr <m.riley@temple.edu>
2021-06-16 17:14:53 -0400

    Cache most recently read rune in Lexer
    Try to prevent too many calls to DecodeRune

--- scan/lexer.go
+++ scan/lexer.go
@@ -27,12 +27,22 @@ type State func(*Lexer) State
 type Lexer struct {
 	tokens   chan *Token
 	input    []byte
-	start    int
 	position int
 	state    State
+
+	current rune
+	peeked  rune
+	width   int
 }
 
 func (l *Lexer) Next() rune {
+	if l.peeked != -1 {
+		l.current = l.peeked
+		l.peeked = -1
+		l.position += l.width
+		return l.current
+	}
+
 	if l.position == len(l.input) {
 		return rune(EOF)
 	}
@@ -42,30 +52,36 @@ func (l *Lexer) Next() rune {
 	// TODO: should probably throw the entire tokenstream out since the
 	// input is garbled
 	// }
+	l.current = r
 	l.position += width
-	return r
+	return l.current
 }
 
 func (l *Lexer) Peek() rune {
+	if l.peeked != -1 {
+		return l.peeked
+	}
+
 	if l.position == len(l.input) {
 		return EOF
 	}
 
-	r, _ := utf8.DecodeRune(l.input[l.position:])
-	return r
+	l.peeked, l.width = utf8.DecodeRune(l.input[l.position:])
+	return l.peeked
 }
 
 func (l *Lexer) Push(t TokenType) {
-	r, _ := utf8.DecodeRune(l.input[l.start:])
-	l.tokens <- &Token{TokenType: t, Value: r}
-	l.start = l.position
+	l.tokens <- &Token{TokenType: t, Value: l.current}
 }
 
+// Lex generates a channel of tokens depending on the initial state
+// given. This channel is closed whenever initState returns nil.
 func Lex(b []byte, initState State) <-chan *Token {
 	l := &Lexer{
 		state:  initState,
 		input:  b,
 		tokens: make(chan *Token),
+		peeked: -1,
 	}
 
 	go func() {
